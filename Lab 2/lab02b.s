e5541fc439388c4b89d0f019ff4ac7bd42e82922
shuaibr <31935226+shuaibr@users.noreply.github.com>
2020-05-30 10:47:12 -0400

    basic register manipulation

---
+++ Lab 2/lab02b.s
@@ -0,0 +1,29 @@
+/*
+-------------------------------------------------------
+lab02b.s
+Assign to and add contents of registers.
+*/
+.equ SWI_Exit, 0x11 @ Assign a label to the terminate program code
+
+@ Copy data from memory to registers
+	LDR	R3, =First
+	LDR	R0, [R3]
+	LDR	R3, =Second
+	LDR	R0, [R3]
+@ Perform arithmetic and store results in memory
+	ADD	R2, R0, R1
+	LDR	R3, =Total
+	STR	R2, [R3]
+	SUB	R2, R0, R1	@ Subtract Second from First
+	LDR	R3, =Diff
+	STR	R2, [R3]
+	
+	SWI	SWI_Exit
+
+.data	@ Define the data section
+
+First:	.word	4
+Second:	.word	8
+
+Total:	.space 4	@ Set aside 4 bytes for total
+Diff:	.space 2	@ Set aside 4 bytes for difference
---
+++ Lab 2/lab02c.s
@@ -0,0 +1,30 @@
+/*
+-------------------------------------------------------
+lab02c.s
+Assign to and add contents of registers.
+
+*/
+.equ SWI_Exit, 0x11 @ Assign a label to the terminate program code
+
+@ Copy contents of first element of Vec1 to Vec2
+	LDR	R0, =Vec1
+	LDR	R1, =Vec2
+	LDR	R2, [R0]
+	STR	R2, [R1]
+@ Copy contents of second element of Vec1 to Vec2
+	ADD	R0, R0, #4
+	ADD	R1, R1, #4
+	LDR	R2, [R0]
+	STR	R2, [R1]
+@ Copy contents of third element of Vec1 to Vec2
+	ADD	R0, R0, #4
+	ADD	R1, R1, #4
+	LDR	R2, [R0]
+	STR	R2, [R1]
+
+	SWI	SWI_Exit	@ Terminate the program
+
+.data	@ Define the data section
+
+Vec1:	.word	1, 2, 3
+Vec2:	.space	6
